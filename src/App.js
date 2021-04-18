ce49ecafce71f43711967720be94f1f6197b389d
Mohit-prog <m98716@gmail.com>
2021-04-19 02:07:01 +0530

    added folder structure and ingredients of burger

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React,{ Component } from "react";
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
-
-function App() {
+class App extends Component {
+  render(){
   return (
     <div >
-     
+     <Layout>
+       <BurgerBuilder></BurgerBuilder>
+     </Layout>
     </div>
   );
+  }
 }
 
 export default App;
