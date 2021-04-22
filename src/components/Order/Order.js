73af424787aef23b2df968706c0c80fc37554db7
Mohit-prog <m98716@gmail.com>
2021-04-22 08:13:37 +0530

    added firebase data to all orders section

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Order.module.css'
 
-const order=props=>(
-    <div className={classes.Order}>
-             <p>ingredients:salad</p>
-             <strong>Price:80Rs</strong>
+const order=props=>{
+    let ingredients=[];
+    for(let ingredientName in props.ingredients){
+        ingredients.push({name:ingredientName ,amount:props.ingredients[ingredientName]})
+    }
+    const ingredientOutput=ingredients.map(ig=>{
+       return (<span 
+       key={ig.name}
+       style={{
+           textTransform:'capitalize',
+           display:'inline-block',
+           margin:'0 8px',
+           border:'1px solid #ccc',
+           padding:'5px'
+       }}
+       >{ig.name}
+        ({ig.amount})</span>)
+    })
+             
+    return(
+    <div 
+    className={classes.Order}>
+             <p>ingredients:{ingredientOutput}</p>
+    <strong>Price:{props.price}Rs</strong>
     </div>
-);
+    
+    )};
 export default order;
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -19,7 +19,7 @@ const withErrorHandler=(WrappedComponent,axios)=>{
         }
 
         componentWillUnmount(){
-            console.log("will unmount",this.resInterceptor,this.reqInterceptor);
+           
             axios.interceptors.request.eject(this.reqInterceptor);
             axios.interceptors.response.eject(this.resInterceptor);
         }
